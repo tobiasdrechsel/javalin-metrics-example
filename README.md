bf6216aadaf1aed9d209008caa41b0243c37060a
ikosaeder <you@example.com>
2019-02-25 23:27:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# javalin-metrics-example
---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    java
+    application
+}
+
+repositories {
+    mavenLocal()
+    jcenter()
+}
+
+dependencies {
+    implementation("io.javalin:javalin:2.6.1-SNAPSHOT")
+    implementation("org.slf4j:slf4j-simple:1.7.25")
+    implementation("io.micrometer:micrometer-core:1.1.3")
+    implementation("io.micrometer:micrometer-registry-prometheus:1.1.3")
+}
+
+application {
+    mainClassName = "javalin.metrics.example.App"
+}
