9c4f0f72de1916a69da68e7d51d17e7ad8fdac26
ikosaeder <you@example.com>
2019-03-24 13:06:06 +0100

    updated javalin + gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 }
 
 dependencies {
-    implementation("io.javalin:javalin:2.6.1-SNAPSHOT")
+    implementation("io.javalin:javalin:2.7.1-SNAPSHOT")
     implementation("org.slf4j:slf4j-simple:1.7.25")
     implementation("io.micrometer:micrometer-core:1.1.3")
     implementation("io.micrometer:micrometer-registry-prometheus:1.1.3")
